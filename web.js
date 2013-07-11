31188b995dc225bd16312862f4aed4664001612c
GitGoingBrains <osterlin@gmail.com>
2013-07-11 20:45:04 +0000

    going on

--- web.js
+++ web.js
@@ -2,11 +2,13 @@ var express = require('express');
 var fs = require('fs')
 var app = express.createServer(express.logger());
 
-var contents = fs.readFileSync('index.html');
+var contentsBuffer = fs.readFileSync('index.html');
+var buffer = new Buffer(contentsBuffer);
+var contents = buffer.toString('utf-8');
 
 app.get('/', function(request, response) {
   response.send(contents);
-};
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
