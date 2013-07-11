9887f6cb67fe2d6079315720ba9fa7f0e533679b
GitGoingBrains <osterlin@gmail.com>
2013-07-11 23:01:08 +0000

    web.js is workin, I think, and I added it.

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
 var express = require('express');
-var fs = require('fs')
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 var contentsBuffer = fs.readFileSync('index.html');
-var buffer = new Buffer(contentsBuffer);
-var contents = buffer.toString('utf-8');
+var content = contentsBuffer.toString('utf-8');
 
 app.get('/', function(request, response) {
-  response.send(contents);
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
