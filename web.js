b9fd698f291e510c035e66e909b5aa2aa96206b6
Ubuntu <ubuntu@ip-10-164-2-84.ec2.internal>
2013-07-11 17:25:39 +0000

    first real try at reading index.html

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
-
+var fs = require('fs')
 var app = express.createServer(express.logger());
 
+var contents = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send(index.html);
-});
+  response.send(contents);
+};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
