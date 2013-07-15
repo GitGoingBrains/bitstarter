25a52a2b6a7d67a229d98fc9ee9ae0d3499ae706
GitGoingBrains <osterlin@gmail.com>
2013-07-15 22:49:44 +0000

    got grader.js work perfectly.  Just need to clean up debug loggers.

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 var rest = require('restler');
 var fs = require('fs');
+var util = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -31,64 +32,101 @@ var URL_DEFAULT = "http://murmuring-brushlands-3582.herokuapp.com/";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-	console.log("%s does not exist. Exiting.", instr);
-	process.exit(1);
-// http://nodejs.org/api/process.html#process_process_exit_code
-    };
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1);
+ // http://nodejs.org/api/process.html#process_process_exit_code
+    }
     return instr;
 };
 
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
+};
+
+if(require.main == module) {
+    program
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <url_string>', 'Path to url of index.html')
+        .parse(process.argv);
+
+    if(program.url != null)   {
+                                                                              console.log('0000');
+	var tempHtml = 'fetched_index.html';
+	                                                                      console.log('0011');
+	var finishLine = function () {
+	    console.log('log out Json now');
+//	    console.log(outJson);
+	};    
+	var getErDone = function(result, response) {
+	    console.error("Wrote to file: %s", tempHtml); 
+	                                                       console.log(tempHtml);                  console.log('0022');
+	    fs.writeFileSync(tempHtml, result);
+	                                                                         console.log('0033');
+	    assertFileExists(tempHtml);
+	                                                                        console.log('0044');
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-// take a URL response and write to file
-var responseHtml = function(result, response) {
-    fs.writeFile(tempHtml, result);
-};
-
-// fetch a URL response
-var fetchHtml = function(url) {
-    rest.get(url).on('complete', responseHtml);
-    return responseHtml;
-};
-
 var loadChecks = function(checksfile) {
-  return JSON.parse(fs.readFileSync(checksfile));
+    return JSON.parse(fs.readFileSync(checksfile));
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
-    for (var ii inchecks) {
-	var present = $(checks[ii]).length > 0;
-	out[checks[ii]] = present;
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+	    
+	    var checkJson = checkHtmlFile(tempHtml, program.checks);
+	                                                                          console.log('0055');
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	                                                                         console.log('0066')
+	    finishLine();  
+	                                                                          console.log('0077');
+	};
+        rest.get(program.url).on('complete', getErDone);
+	                                                                       console.log('1111');
+    } else {
+
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
     }
     return out;
 };
 
-var clone = function(fn) {
-    return fn.bind({});
-};
 
-if(require.main == module) {
-    program
-	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-	.option('-u, --url <url_string>', 'Path to url of index.html')
-	.parse(process.argv);
-    
-    if(program.url != null)   {
-	var tempHtml = 'fetched_index.html';
-	fetchhtml(program.url);
-	var checkJson = checkHtmlFile(tempHtml, program.checks);
-    } else {
-	var checkJson = checkHtmlFile(program.file, program.checks);
+
+
+
+
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
     };
-
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
-};
+}
